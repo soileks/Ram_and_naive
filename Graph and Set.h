2842dc2f898f235e75144cc341bb5699ebd55101
Maksim <redbulprofi@gmail.com>
2023-10-05 21:41:54 +0300

    Добавил cpp файлы

--- Graph and Set.h
+++ Graph and Set.h
@@ -13,60 +13,10 @@ class Graph {
     vector<vector<int>> E;
 
 public:
-  
 
+    vector<vector<int>> Generate_graph(int _n, int _m);
 
-   vector<vector<int>> Generate_graph(int _n, int _m) {
-        m = _m;
-        n = _n;
-        //Генерация случайного графа
-        vector<vector<int>> e(m, vector<int>(2));
-        E = e;
-
-        for (int j = 0; j < m; j++) {
-            int a = rand() % n;
-            int b = rand() % n;
-            //Обеспечивает, чтобы рёбра не повторялись
-            while (find(E.begin(), E.end(), vector<int>{a, b}) != E.end() || find(E.begin(), E.end(), vector<int>{b, a}) != E.end() || a == b) {//find возвращает указатель на конец, если не нашли
-                a = rand() % n;
-                b = rand() % n;
-            }
-            E[j] = { a, b };
-        }
-        
-        return E;
-    }
-
-
-
-    void print_graph() {
-        //Вывод графа в виде матрицы смежности
-        if (n <= 10) {
-            cout << "Граф задан матрицей смежности:" << endl;
-            int** matrix = new int* [n];
-            for (int i = 0; i < n; i++)
-                matrix[i] = new int[n];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++)
-                {
-                    matrix[i][j] = 0;
-                }
-            }
-            for (int i = 0; i < m; i++) {
-                matrix[E[i][0]][E[i][1]] = 1;
-                matrix[E[i][1]][E[i][0]] = 1;
-            }
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++)
-                {
-                    cout << matrix[i][j] << " ";
-                }
-                cout << endl;
-
-            }
-        }
-    }
-
+    void print_graph();
 
 };
 
@@ -74,31 +24,14 @@ class Set {
     vector<int> col;
 
 public:
-    Set(int n) {
-        
-        vector<int> _c(n);
-        col = _c;
-    }
+    Set(int n);
+
+    void create(int x);
     
-    void create(int x) { //col это коллекция
-        
-        col[x] = x;
-    }
+    int search(int x);
 
-    int search(int x) {
-        return col[x];
-    }
+    void join(int x, int y);
 
-    void join(int x, int y) {
-        int z = min(x, y);
-        for (int i = 0; i < col.size(); i++) {
-            if (col[i] == x || col[i] == y) {
-                col[i] = z;//принадлежность множеству
-            }
-        }
-    }
-    void Reinitialization(vector<int> _c) {
-        col = _c;
-    }
+    void Reinitialization(vector<int> _c);
 
 };
--- task4.h
+++ task4.h
@@ -1,31 +1,3 @@
 #pragma once
 #include"task1.h"
-void task4() {
-
-        vector<int> M;
-        int tmp = 0;
-        for (int i = 1; i <= 1002; i += 10) {
-            int n = i;
-            int m = 0;
-            vector<int> comp(n);
-            vector<int> comp_connected(n);
-           
-            vector<vector<int>> E;
-            array_RAM(comp, E, n, m);
-            while (comp != comp_connected) {
-                m++;
-                int a = rand() % n;
-                int b = rand() % n;
-                while ((find(E.begin(), E.end(), vector<int>{a, b}) != E.end()) || (find(E.begin(), E.end(), vector<int>{b, a}) != E.end()) || (a == b)) {
-                    a = rand() % n;
-                    b = rand() % n;
-                }
-                E.push_back({ a, b });
-                array_RAM(comp, E, n, m);
-            }
-            M.push_back(m);
-           cout << i << endl;
-            cout << m << endl;
-            tmp++;
-        }
-}
+void task4();
